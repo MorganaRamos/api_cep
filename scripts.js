b5a358de3b66c3e24b427a5c4fe02685336e5c9c
MorganaRamos <morganaramos@edu.unifor.br>
2023-06-01 22:01:09 -0300

    for each

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
 const encontrarEstados = () => {
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then(response => response.json())
+        .then(json => {
 
+            let inputEstado = document.getElementById('estado')
+            let estados = ''
+            json.forEach(estado => {
+                estados += `<option value=${estado.sigla}>${estado.nome}</option>´
+               
+                );
+                console.log(estados)
+                inputEstado.innerHTML = estados
+               
+            )
+            }
+              
 }
-
-encontrarEstados()
