36892d8f2da7dcba331c7c6d620dc6c755eb3cc8
MorganaRamos <morganaramos@edu.unifor.br>
2023-06-01 21:23:40 -0300

    função encontrar estados

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const encontrarEstados = () => {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
+}
